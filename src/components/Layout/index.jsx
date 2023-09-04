8aa90d1390c9924f68994ff27da40b4086f83548
Ishaan Shinde <ishaanshinde08@gmail.com>
2023-09-05 04:19:55 +0530

    made the layout component and implemented browser router

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,6 @@
+import './index.scss'
+
+const Layout = () => {
+    return <>testdesu</>
+}
+export default Layout
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+/* to navigate through routes */
+import { BrowserRouter } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
