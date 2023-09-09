c2fa2411470dad40177ff29f1946b0ad565c072f
Ishaan Shinde <ishaanshinde08@gmail.com>
2023-09-09 19:59:31 +0530

    nav bar complete: added social links + icons

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -3,6 +3,7 @@ import './index.scss'
 import logo from '../../assets/images/logo-flux-echo.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faGitlab, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     
@@ -13,7 +14,7 @@ const Sidebar = () => (
                 <img src={logo} alt='logo' />
             </Link>
 
-            {/* nan bar elements */}
+            {/* nan bar main elements - HOME ABOUT CONTACT*/}
             <nav>
                 <NavLink exact='true' activeclassname='active' className='home-link' to='/'>
                     <FontAwesomeIcon icon={faHome} color='#ff8800' />
@@ -27,6 +28,27 @@ const Sidebar = () => (
                     <FontAwesomeIcon icon={faEnvelope} color='#ff8800' />
                 </NavLink>
             </nav>
+
+            {/* nav bar normal elements - GITLAB GITHUB LINKEDIN YOUTUBE */}
+            <ul>
+                <li><a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/ishaanshinde/'>
+                    <FontAwesomeIcon icon={faLinkedin} color='#ff8800' />
+                </a></li>
+
+                <li><a target='_blank' rel='noreferrer' href="https://gitlab.com/users/IshaanShinde/projects">
+                    <FontAwesomeIcon icon={faGitlab} color='#ff8800' />
+                </a></li>
+
+                <li><a target='_blank' rel='noreferrer' href="https://github.com/IshaanShinde">
+                    <FontAwesomeIcon icon={faGithub} color='#ff8800' />
+                </a></li>
+
+                <li><a target='_blank' rel='noreferrer' href="https://www.youtube.com/@psychedelicloli">
+                    <FontAwesomeIcon icon={faYoutube} color='#ff8800' />
+                </a></li>
+                
+
+            </ul>
         </div>
     
 )
