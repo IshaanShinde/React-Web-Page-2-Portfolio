22f2629dd757ad16f325a93c85b8c8c2ece7e2d1
Ishaan Shinde <ishaanshinde08@gmail.com>
2023-09-10 12:37:17 +0530

    added animated letters component + font-size addjustments made
    error in home component while using the animation for <h1>, temporarily disabled

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom"
 import './index.scss'
+import AnimatedLetters from "../AnimatedLetters"
 
 const Home = () => {
+
+    /* const [letterClass, setLetterClass] = useState('text-animate')/* 
+    const hiArray = ['K','o','n','n','i','c','h','i','w','a','!',' ','>','w', '<'] */
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Konnichiwa! &gt;w&lt;</h1>
+                <h1>
+                    {/* <AnimatedLetters letterClass={letterClass} strArray={hiArray} idx={15} /> */}
+                    Konnichiwa! &gt;w&lt;
+                </h1>
+
                 <h2 className="indent-forward">I'm Ishaan Shinde,<br />Software Developer and Audio Engineer</h2>
                 <Link to='/contact' className="flat-button indent-forward-two">&lt;contact&gt;</Link>
             </div>
