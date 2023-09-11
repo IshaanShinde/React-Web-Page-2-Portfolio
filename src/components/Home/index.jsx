22550e0a00494250994a7c7bf081b20070f61684
Ishaan Shinde <ishaanshinde08@gmail.com>
2023-09-11 10:52:20 +0530

    fixed error in home component for animations, now enabled

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,22 +1,25 @@
 import { Link } from "react-router-dom"
 import './index.scss'
 import AnimatedLetters from "../AnimatedLetters"
+import { useState, useEffect } from "react"
 
 const Home = () => {
 
-    /* const [letterClass, setLetterClass] = useState('text-animate')/* 
-    const hiArray = ['K','o','n','n','i','c','h','i','w','a','!',' ','>','w', '<'] */
-
+    const [letterClass, setLetterClass] = useState('text-animate') 
+    const hiArray = ['K','o','n','n','i','c','h','i','w','a','!',' ','>','w', '<']
+    const contactArray = ['<', 'c', 'o', 'n', 't', 'a', 'c', 't', '>']
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
-                    {/* <AnimatedLetters letterClass={letterClass} strArray={hiArray} idx={15} /> */}
-                    Konnichiwa! &gt;w&lt;
+                    <AnimatedLetters letterClass={letterClass} strArray={hiArray} idx={15} />
+                    {/* Konnichiwa! &gt;w&lt; */}
                 </h1>
 
                 <h2 className="indent-forward">I'm Ishaan Shinde,<br />Software Developer and Audio Engineer</h2>
-                <Link to='/contact' className="flat-button indent-forward-two">&lt;contact&gt;</Link>
+                <Link to='/contact' className="flat-button indent-forward-two">
+                    <AnimatedLetters letterClass={letterClass} strArray={contactArray} idx={30} />
+                </Link>
             </div>
         </div>
     )
