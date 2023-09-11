12757d91d76a05471b4eb8d508a047207d97e4f3
Ishaan Shinde <ishaanshinde08@gmail.com>
2023-09-11 16:56:01 +0530

    added contact component + fixed fade in issues on reload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
+import Contact from './components/Contact'
 /* 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
 
         </Route>
       </Routes>
---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,65 @@
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+
+const Contact = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    /* useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, []) */
+
+
+    return (
+        <>
+            <div className='container contact-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters 
+                            letterClass={letterClass}
+                            strArray={['C','o','n','t','a','c','t']}
+                            idx={15}
+                        />
+                    </h1>
+                    <p /* className='indent-forward' */>
+                        Feel free to reach out for job opportunities in software development and audio production
+                    </p>
+                    <p /* className='indent-forward' */>
+                        and guitar lessons as well &gt;w&lt;
+                    </p>
+
+                    <div className='contact-form'>
+                        <form>
+                            <ul>
+                                <li className='half'>
+                                    <input type='text' name='name' placeholder='Enter Your Name' required />
+                                </li>
+
+                                <li className='half'>
+                                    <input type='email' name='email' placeholder='Enter Your Email' required />
+                                </li>
+
+                                <li>
+                                    <input type='text' name='subject' placeholder='Subject' required />
+                                </li>
+
+                                <li>
+                                    <textarea name="message" placeholder='Message' required></textarea>
+                                </li>
+
+                                <li>
+                                    <input type="submit" className='flat-button' value='SEND'/>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Contact
